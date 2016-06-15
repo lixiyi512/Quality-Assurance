e9b02301f348355f7a3cf82b1f19de951c3ff6cb
Xiyi Li <lixiyi512@gmail.com>
2016-06-15 15:50:39 -0400

    Add initial files

--- Deliverable_2/BusinessPerson.java
+++ Deliverable_2/BusinessPerson.java
@@ -1,20 +1,20 @@
 public class BusinessPerson extends Visitor {
 		
-		public BusinessPerson(int id){
-			super(id);
-		}
-		
-		public void showType(){
-			System.out.println("Visitor " + id + " is a Business Person.");
-		}
-		
-		public boolean preference(int loc){
-			boolean like = false;
-			if(loc == LOC_SH || loc == LOC_DT){
-				like = true;
-			} else{
-				like = false;
-			} return like;
-		}
-	
+	public BusinessPerson(int id){
+		super(id);
 	}
+	
+	public void showType(){
+		System.out.println("Visitor " + id + " is a Business Person.");
+	}
+	
+	public boolean preference(int loc){
+		boolean like = false;
+		if(loc == LOC_SH || loc == LOC_DT){
+			like = true;
+		} else{
+			like = false;
+		} return like;
+	}
+	
+}
